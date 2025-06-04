0725396a8ebc4e8a0fa329f386c6d0bed112108b
syelii12 <117338933+syelii12@users.noreply.github.com>
2025-06-04 09:53:10 +0700

    Progres2
    baru bikin per level

---
+++ Assets/Scripts/KnockBack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    public bool GettingKnockedBack { get; private set; }
+
+    [SerializeField] private float knockBackTime = .2f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void GetKnockedBack(Transform damageSource, float knockBackThrust)
+    {
+        GettingKnockedBack = true;
+        Vector2 difference = (transform.position - damageSource.position).normalized * knockBackThrust * rb.mass;
+        rb.AddForce(difference, ForceMode2D.Impulse);
+        StartCoroutine(KnockRoutine());
+    }
+
+    private IEnumerator KnockRoutine()
+    {
+        yield return new WaitForSeconds(knockBackTime);
+        rb.velocity = Vector2.zero;
+        GettingKnockedBack = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public int damage = 20;
+    public float knockbackForce = 5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerMovement player = collision.GetComponent<PlayerMovement>();
+        if (player != null)
+        {
+            Vector2 knockbackDir = (collision.transform.position - transform.position).normalized;
+            player.TakeDamage(damage, knockbackDir);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
